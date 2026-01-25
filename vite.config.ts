c787634c8be868e9a6548b16bc86f4d35176d6ab
vickbk <vickbake8@gmail.com>
2026-01-25 21:08:46 +0200

    adding core configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/interactive-comments-section/",
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
     }),
   ],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/vitest-setup.ts",
+    globals: true,
+    coverage: {
+      reporter: ["text", "json", "html"],
+      exclude: ["src/tests/*", "src/mocks/*"],
+    },
+  },
+});
