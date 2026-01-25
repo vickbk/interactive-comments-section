1f274868432a3a27afc3095a622caf03ba04ecca
vickbk <vickbake8@gmail.com>
2026-01-25 21:27:20 +0200

    setting css configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 /// <reference types="vitest/config" />
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
@@ -12,6 +13,7 @@ export default defineConfig({
         plugins: [["babel-plugin-react-compiler"]],
       },
     }),
+    tailwindcss(),
   ],
   resolve: {
     alias: {
