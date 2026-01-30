f9d460d0c850018b2c2236c866678735973a5605
vickbk <vickbake8@gmail.com>
2026-01-30 14:28:29 +0200

    adding random generate function and randomly setting current user or not

--- src/app/comment/components/action-buttons.tsx
+++ src/app/comment/components/action-buttons.tsx
@@ -1,16 +1,31 @@
+import { getRandomElement } from "@/libs";
 import { ActionBtn } from "./btn";
 
 export const ActionButtons = () => {
+  const isCurrentUser = getRandomElement([true, false]);
   return (
-    <div className="flex flex-wrap gap-2 font-semibold">
-      <ActionBtn color="c-pink-400" text="Delete" icon="trash-fill" />
-      <ActionBtn color="c-purple-600" text="Edit" icon="pencil-fill" />
-      <ActionBtn
-        color="c-purple-600"
-        text="Reply"
-        srText="to post"
-        icon="reply-fill"
-      />
+    <div className="flex flex-wrap gap-2 font-semibold justify-end">
+      {isCurrentUser ? (
+        <>
+          <ActionBtn
+            color="c-pink-400 active-c-pink-200"
+            text="Delete"
+            icon="trash-fill"
+          />
+          <ActionBtn
+            color="c-purple-600 active-c-purple-200"
+            text="Edit"
+            icon="pencil-fill"
+          />
+        </>
+      ) : (
+        <ActionBtn
+          color="c-purple-600 active-c-purple-200"
+          text="Reply"
+          srText="to post"
+          icon="reply-fill"
+        />
+      )}
     </div>
   );
 };
