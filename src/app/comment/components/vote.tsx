461ce2427a2030231593c453598fd12f3676827c
vickbk <vickbake8@gmail.com>
2026-01-30 14:00:23 +0200

    styling vote element

--- src/app/comment/components/vote.tsx
+++ src/app/comment/components/vote.tsx
@@ -1,13 +1,27 @@
-import { Article, Heading } from "@/components/shared";
+import { Article, Heading, SROnly } from "@/components/shared";
 import { VoteBtn } from "./vote-btn";
 
 export const Vote = () => {
   return (
-    <Article>
-      <Heading className="sr-only">Vote for this post</Heading>
-      <VoteBtn />
-      <span>12</span>
-      <VoteBtn />
+    <Article className="justify-self-start">
+      <Heading className="sr-only">Vote for this comment</Heading>
+      <dl className="flex gap-4 c-purple-600 items-center background rounded-lg">
+        <dt className="sr-only">Up vote</dt>
+        <dd>
+          <VoteBtn />
+        </dd>
+        <dt className="sr-only">Votes</dt>
+        <dd>
+          <span>
+            <SROnly>Current votes:</SROnly>12
+          </span>
+        </dd>
+
+        <dt className="sr-only">Down vote</dt>
+        <dd>
+          <VoteBtn icon="dash" />
+        </dd>
+      </dl>
     </Article>
   );
 };
